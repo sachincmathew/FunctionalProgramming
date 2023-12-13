71273584bcc9a1349da27ccf2dca594e80df9377
sachincmathew <sachincmathew@gmail.com>
2023-12-14 00:12:43 +0800

    usage of map() to perform an operation on each element

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -16,15 +16,47 @@ public class FP01Functional {
 
         System.out.println("--------------------------------------------");
 
-        printAllCourses(List.of("Spring","Spring Boot,","API" ,"Microservices","AWS","PCF","Azure","Docker","Kubernetes"));
+        printAllCourses(List.of("Spring", "Spring Boot,", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                "Kubernetes"));
 
         System.out.println("--------------------------------------------");
 
-        printCoursesWithSpring(List.of("Spring","Spring Boot,","API" ,"Microservices","AWS","PCF","Azure","Docker","Kubernetes"));
+        printCoursesWithSpring(
+                List.of("Spring", "Spring Boot,", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                        "Kubernetes"));
 
         System.out.println("--------------------------------------------");
 
-        printCoursesWithMin4Letters(List.of("Spring","Spring Boot,","API" ,"Microservices","AWS","PCF","Azure","Docker","Kubernetes"));
+        printCoursesWithMin4Letters(
+                List.of("Spring", "Spring Boot,", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                        "Kubernetes"));
+
+        System.out.println("--------------------------------------------");
+
+        printSquaresOfEvenNumbers(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+
+        System.out.println("--------------------------------------------");
+
+        printCubesOfOddNumbers(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+
+        System.out.println("--------------------------------------------");
+
+        printNumberOfCharsInCourse(
+                List.of("Spring", "Spring Boot,", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                        "Kubernetes"));
+    }
+
+    private static void printNumberOfCharsInCourse (List<String> courses) {
+        courses.stream().map(course -> course + " " + course.length()).forEach(System.out::println);
+        courses.stream().map(String::length).forEach(System.out::println);
+    }
+
+    private static void printCubesOfOddNumbers (List<Integer> integers) {
+        integers.stream().filter(number -> number % 2 != 0).map(number -> number * number * number).forEach(System.out::println);
+    }
+
+    private static void printSquaresOfEvenNumbers (List<Integer> integers) {
+        integers.stream().filter(number -> number % 2 == 0).map(number -> number * number).forEach(System.out::println);
     }
 
     private static void printCoursesWithMin4Letters (List<String> spring) {
