08885fc15442b41bfe4d18c3a2871dfe78074ac4
sachincmathew <sachincmathew@gmail.com>
2023-12-13 23:56:45 +0800

    printing even numbers from list by using .filter()

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -5,9 +5,19 @@ import java.util.List;
 public class FP01Functional {
     public static void main (String[] args) {
         printAllNumbersInListFunctional (List.of (12, 9, 13, 4, 6, 2, 4, 12, 15));
+
+        System.out.println ("--------------------------------------------");
+
+        printEvenNumbersInListFunctional (List.of (12, 9, 13, 4, 6, 2, 4, 12, 15));
+    }
+
+    private static void printEvenNumbersInListFunctional (List<Integer> integers) {
+        integers.stream()
+                .filter (number -> number % 2 == 0)
+                .forEach (System.out::println);
     }
 
     private static void printAllNumbersInListFunctional (List<Integer> integers) {
-        integers.stream().forEach (System.out::print);//Method Reference
+        integers.stream().forEach (System.out::println);//Method Reference
     }
 }
